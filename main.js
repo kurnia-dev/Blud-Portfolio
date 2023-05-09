c300099bb0d962f712dabce705fb9c74acdb6590
Kurnia <kurniawanviic54@gmail.com>
2023-05-09 11:30:38 +0700

    feat : finish experiences section
    the tab has already working. if user clicked on each tab, it will show the tab content. first tab shown by default

--- main.js
+++ main.js
@@ -43,3 +43,110 @@ for (let i in skills) {
     `
     skillsContainer.append(li)
 }
+
+
+// Tab Experience
+const tabsTitle = {
+    1: {
+        company: "Google", 
+        role: "UI Engineer", 
+        img: "res/icon/google.svg", 
+    }, 
+    2: {
+        company: "Hashnode", 
+        role: "Frontend Developer", 
+        img: "res/icon/hashnode.svg", 
+    }, 
+    3: {
+        company: "Oculus", 
+        role: "Frontend Developer", 
+        img: "res/icon/oculus.svg", 
+    }, 
+}
+
+const tabsContent = {
+    1: {
+        role    : "UI Engineer", 
+        company :"Google",
+        year    : "2022 - Present", 
+        jobdesc : {
+            1: "Design and implementation of visually appealing and intuitive user interfaces for innovative technology products. ", 
+            2: "Continuous improvement of the user experience and maintenance of quality standards.", 
+            3: "Conducting testing and validation, user research, and collaborating with other teams to ensure quality and effective integration with the final product."
+        }
+    },
+    2: {
+        role    : "Frontend Developer", 
+        company : "Hashnode",
+        year    : "2018 - 2022", 
+        jobdesc : {
+            1: "Continuous improvement of the user experience and maintenance of quality standards.", 
+            2: "Design and implementation of visually appealing and intuitive user interfaces for innovative technology products. ", 
+            3: "Conducting testing and validation, user research, and collaborating with other teams to ensure quality and effective integration with the final product."
+        }
+    },
+    3: {
+        role    : "Frontend Developer", 
+        company : "Oculus", 
+        year    : "2015 - 2018", 
+        jobdesc : {
+            1: "Conducting testing and validation, user research, and collaborating with other teams to ensure quality and effective integration with the final product.",
+            2: "Continuous improvement of the user experience and maintenance of quality standards.", 
+            3: "Design and implementation of visually appealing and intuitive user interfaces for innovative technology products. ", 
+        }
+    }
+}
+
+const tabTitleContainer = document.querySelector('.tab-title-container')
+
+for (let i in tabsTitle) {
+    let div = document.createElement('div')
+    div.classList.add('tab-title')
+    div.setAttribute('id', i)
+    div.innerHTML = `
+        <img src="${tabsTitle[i].img}" alt="" />
+        <div class="title">
+            <big>${tabsTitle[i].company}</big><br />
+            <small>${tabsTitle[i].role}</small>
+        </div>
+    `
+    if (i == 1) div.classList.add('active') 
+    tabTitleContainer.append(div)
+}
+
+
+const tabContentElement = document.querySelector('.tab-content')
+const tabContentDescription = document.querySelector('.tab-content-description')
+
+showTabContent()
+
+tabTitleContainer.addEventListener('click', (e) => {
+    for (let child of tabTitleContainer.children) {
+        child.classList.remove('active')
+    }
+    e.target.classList.add('active')
+
+    let id = e.target.getAttribute('id')
+
+    showTabContent(id)
+})
+
+function showTabContent(id = 1) {
+    let ul = document.createElement('ul')
+    ul.classList.add('tab-content-description')
+
+    for (let i in tabsContent[id].jobdesc) {
+        let li = document.createElement('li') 
+        li.innerHTML = tabsContent[id].jobdesc[i]
+        ul.append(li)
+    }
+
+    tabContentElement.innerHTML = `
+        <big>${tabsContent[id].role} at <span>${tabsContent[id].company}</span></big>
+        <br />
+        <small>${tabsContent[id].year}</small>
+    `
+
+    tabContentElement.append(ul)
+}
+// End of tab
